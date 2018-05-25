3069f5c82f34754b9d3c4945844de5398e51f488
denis5417 <denisz.pol@yandex.ru>
2018-05-25 23:12:53 +0300

    added tracking changes to the profile. speed of operation is markedly reduced (

--- airtabledbbot.py
+++ airtabledbbot.py
@@ -4,7 +4,7 @@ from time import sleep
 import vk_api
 from airtable import Airtable
 
-friends = []
+friends = {}
 
 
 def at_auth(base_key, table_name, api_key):
@@ -17,31 +17,64 @@ def vk_auth(login, password):
     return vk
 
 
-def new_friend_added(vk):
-    try:
-        new_friend = vk.method('friends.getRecent', {'count': 1})[0]
-    except IndexError:
-        return None
-    if new_friend not in friends:
-        friends.append(new_friend)
-        return new_friend
+def parse_table(airtable):
+    friends = {}
+    for page in airtable.get_iter():
+        for record in page:
+            friends[int(record['fields']['Ссылка на профиль'].replace(
+                'vk.com/id', ''))] = record['id']
+    return friends
 
 
-def get_new_friend_info(vk, new_friend):
+def get_friend_ids(vk):
+    return vk.method('friends.get')['items']
+
+
+def update_friends(vk, at, friend_ids, friends):
+    for id in friend_ids:
+        try:
+            diff = get_diff(get_friend_info(vk, id),
+                            at.get(friends[id])['fields'])
+            if diff:
+                at.update(friends[id], diff)
+        except KeyError:
+            friends[id] = push_vk_info_to_at(at, get_friend_info(vk, id))
+        except TypeError:
+            pass
+
+
+def get_diff(vk_info, at_info):
+    diff = {}
+    if vk_info['name'] != at_info['Имя']:
+        diff['Имя'] = vk_info['name']
+    if vk_info['mobile_phone'] != at_info['Мобильный телефон']:
+        diff['Мобильный телефон'] = vk_info['mobile_phone']
+    if vk_info['home_phone'] != at_info['Домашний телефон']:
+        diff['Домашний телефон'] = vk_info['home_phone']
+    if vk_info['bdate'] != at_info['Дата рождения']:
+        diff['Дата рождения'] = vk_info['bdate']
+    if vk_info['city'] != at_info['Город']:
+        diff['Город'] = vk_info['city']
+    return diff
+
+
+def get_friend_info(vk, new_friend):
     info = {}
     info['add_time'] = dt.now().strftime('%d.%m.%Y %H:%M')
     resp = vk.method(
         'users.get', {
             'user_ids': new_friend, 'fields': 'contacts, bdate, city'
-            })[0]
+        })[0]
     info['name'] = '{} {}'.format(resp['first_name'], resp['last_name'])
     info['link'] = 'vk.com/id{}'.format(resp['id'])
     try:
-        if resp['mobile_phone'] == '' or not all(n.isnumeric() for n in resp['mobile_phone']):
+        if resp['mobile_phone'] == '' or not all(
+                n.isnumeric() for n in resp['mobile_phone']):
             info['mobile_phone'] = 'Не указан'
         else:
             info['mobile_phone'] = resp['mobile_phone']
-        if resp['home_phone'] == '' or not all(n.isnumeric() for n in resp['home_phone']):
+        if resp['home_phone'] == '' or not all(
+                n.isnumeric() for n in resp['home_phone']):
             info['home_phone'] = 'Не указан'
         else:
             info['home_phone'] = resp['home_phone']
@@ -64,7 +97,7 @@ def get_new_friend_info(vk, new_friend):
 
 
 def push_vk_info_to_at(airtable, info):
-    airtable.insert({
+    return airtable.insert({
         'Имя': info['name'],
         'Ссылка на профиль': info['link'],
         'Мобильный телефон': info['mobile_phone'],
@@ -76,13 +109,11 @@ def push_vk_info_to_at(airtable, info):
 
 
 def main():
-    vk = vk_auth('zvyagdenis@gmail.com', '8ed0faf2')
-    airtable = at_auth('appJRVy4Zj1DEmS29', 'New friends', 'keytbJxjeLWqOnrWD')
+    vk = vk_auth('*login*', '*password*')
+    airtable = at_auth('*base_id*', '*table_name*', '*api_key*')
+    friends = parse_table(airtable)
     while True:
-        sleep(1)
-        new_friend = new_friend_added(vk)
-        if(new_friend):
-            push_vk_info_to_at(airtable, get_new_friend_info(vk, new_friend))
+        update_friends(vk, airtable, get_friend_ids(vk), friends)
 
 
 if __name__ == '__main__':
