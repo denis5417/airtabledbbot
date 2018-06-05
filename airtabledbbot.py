b92257e814a19c981753679dbe64fcfa558cce54
denis5417 <denisz.pol@yandex.ru>
2018-06-05 18:52:53 +0300

    commit

--- airtabledbbot.py
+++ airtabledbbot.py
@@ -1,10 +1,10 @@
+import re
 from datetime import datetime as dt
-from time import sleep
+from time import sleep, time
 
+import requests
 import vk_api
 from airtable import Airtable
-
-friends = {}
 
 
 def at_auth(base_key, table_name, api_key):
@@ -17,103 +17,162 @@ def vk_auth(login, password):
     return vk
 
 
-def parse_table(airtable):
+def parse_table(airtable, vk):
     friends = {}
-    for page in airtable.get_iter():
-        for record in page:
-            friends[int(record['fields']['Ссылка на профиль'].replace(
-                'vk.com/id', ''))] = record['id']
+    for record in airtable.get_all(view='Таблица Денис'):
+        if 'Профиль в соцсети' not in record['fields']:
+            continue
+        vk_id = re.search(
+            r'(?<=vk.com/)[0-9a-zA-Z_.]*',
+            record['fields']['Профиль в соцсети'])
+        if vk_id is None:
+            continue
+        else:
+            vk_id = vk_id[0]
+        if vk_id == 'im':
+            vk_id = re.search(
+                r'(?<=sel=)[0-9a-zA-Z_.]*',
+                record['fields']['Профиль в соцсети'])[0]
+        try:
+            vk_id = vk.method(
+                'users.get', {
+                    'user_ids': vk_id, 'fields': '',
+                    'lang': 'ru'
+                })[0]['id']
+            friends[vk_id] = record['id']
+        except (requests.exceptions.ReadTimeout,
+                requests.exceptions.HTTPError,
+                vk_api.exceptions.ApiHttpError,
+                vk_api.exceptions.ApiError):
+            pass
+    friends[vk_id] = record['id']
     return friends
 
 
-def get_friend_ids(vk):
-    return vk.method('friends.get')['items']
+def get_all_friend_ids(vk):
+    try:
+        return vk.method('friends.get', {'count': 100000})['items']
+    except (requests.exceptions.ReadTimeout, requests.exceptions.HTTPError,
+            vk_api.exceptions.ApiHttpError) as err:
+        print(err)
 
 
 def update_friends(vk, at, friend_ids, friends):
-    for id in friend_ids:
+    if friend_ids is not None:
+        for id in friend_ids:
+            if id not in friends:
+                friends[id] = push_vk_info_to_at(at, get_friend_info(vk, id))
+    for id in friends:
         try:
-            diff = get_diff(get_friend_info(vk, id),
-                            at.get(friends[id])['fields'])
-            if diff:
+            vk_info = get_friend_info(vk, id)
+            at_info = at.get(friends[id])
+        except (requests.exceptions.ReadTimeout, requests.exceptions.HTTPError,
+                vk_api.exceptions.ApiHttpError) as err:
+            print(err)
+            continue
+        if vk_info and at_info:
+            diff = get_diff(vk_info, at_info['fields'])
+        if diff:
+            try:
                 at.update(friends[id], diff)
-        except KeyError:
-            friends[id] = push_vk_info_to_at(at, get_friend_info(vk, id))
-        except TypeError:
-            pass
+            except (requests.exceptions.HTTPError,
+                    requests.exceptions.ReadTimeout) as err:
+                print(err)
+
+
+def check_info(field, at_info, vk_field):
+    return vk_field is not None \
+        and ((field not in at_info or
+              vk_field != at_info[field]) and vk_field is not None)
 
 
 def get_diff(vk_info, at_info):
     diff = {}
-    if vk_info['name'] != at_info['Имя']:
-        diff['Имя'] = vk_info['name']
-    if vk_info['mobile_phone'] != at_info['Мобильный телефон']:
-        diff['Мобильный телефон'] = vk_info['mobile_phone']
-    if vk_info['home_phone'] != at_info['Домашний телефон']:
-        diff['Домашний телефон'] = vk_info['home_phone']
-    if vk_info['bdate'] != at_info['Дата рождения']:
+    check_info('ФИО', at_info, vk_info['name'])
+    if 'name' in vk_info and check_info('ФИО', at_info, vk_info['name']):
+        diff['ФИО'] = vk_info['name']
+    if 'phone' in vk_info and check_info('Телефон', at_info, vk_info['phone']):
+        diff['Телефон'] = vk_info['phone']
+    if 'bdate' in vk_info and \
+            check_info('Дата рождения', at_info, vk_info['bdate']):
         diff['Дата рождения'] = vk_info['bdate']
-    if vk_info['city'] != at_info['Город']:
+    if 'bdate_age' in vk_info and \
+            check_info('Возраст', at_info, vk_info['bdate_age']):
+        diff['Возраст'] = vk_info['bdate_age']
+    if 'city' in vk_info and check_info('Город', at_info, vk_info['city']):
         diff['Город'] = vk_info['city']
     return diff
 
 
 def get_friend_info(vk, new_friend):
     info = {}
-    info['add_time'] = dt.now().strftime('%d.%m.%Y %H:%M')
-    resp = vk.method(
-        'users.get', {
-            'user_ids': new_friend, 'fields': 'contacts, bdate, city'
-        })[0]
+    try:
+        resp = vk.method(
+            'users.get', {
+                'user_ids': new_friend, 'fields': 'contacts, bdate, city',
+                'lang': 'ru'
+            })[0]
+    except (requests.exceptions.ReadTimeout, requests.exceptions.HTTPError,
+            vk_api.exceptions.ApiHttpError) as err:
+        print(err)
+        info['name'] = None
+        info['link'] = None
+        info['phone'] = None
+        info['city'] = None
+        info['bdate'] = None
+        return info
     info['name'] = '{} {}'.format(resp['first_name'], resp['last_name'])
     info['link'] = 'vk.com/id{}'.format(resp['id'])
+    info['phone'] = ''
+    if 'mobile_phone' in resp and all(
+            n.isnumeric() for n in resp['mobile_phone']):
+        info['phone'] += resp['mobile_phone'] + ' '
+    if 'home_phone' in resp and all(
+            n.isnumeric() for n in resp['home_phone']):
+        info['phone'] += resp['home_phone']
+    if info['phone'].strip() == '':
+        info['phone'] = None
+    info['city'] = resp['city']['title'] if 'city' in resp else None
+    if 'bdate' not in resp:
+        info['bdate'] = None
+        info['bdate_age'] = None
+        return info
     try:
-        if resp['mobile_phone'] == '' or not all(
-                n.isnumeric() for n in resp['mobile_phone']):
-            info['mobile_phone'] = 'Не указан'
-        else:
-            info['mobile_phone'] = resp['mobile_phone']
-        if resp['home_phone'] == '' or not all(
-                n.isnumeric() for n in resp['home_phone']):
-            info['home_phone'] = 'Не указан'
-        else:
-            info['home_phone'] = resp['home_phone']
-    except KeyError:
-        info['mobile_phone'] = 'Информация скрыта'
-        info['home_phone'] = 'Информация скрыта'
-    try:
-        info['city'] = resp['city']['title']
-    except KeyError:
-        info['city'] = 'Не указан'
-    try:
-        bdate = dt.strptime(resp['bdate'], '%d.%m.%Y').strftime('%d.%m.%Y')
+        resp['bdate'] = resp['bdate'].replace('29.2', '28.2')
+        bdate = dt.strptime(resp['bdate'], '%d.%m.%Y').strftime(
+            '%-m.%d') + '.' + str(dt.now().year)
         info['bdate'] = bdate
-    except KeyError:
-        info['bdate'] = 'Не указана'
+        bdate_age = dt.strptime(
+            resp['bdate'], '%d.%m.%Y').strftime('%-m.%d.%Y')
+        info['bdate_age'] = bdate_age
     except ValueError:
-        bdate = dt.strptime(resp['bdate'], '%d.%m').strftime('%d.%m')
+        bdate = dt.strptime(resp['bdate'], '%d.%m').strftime(
+            '%-m.%d') + '.' + str(dt.now().year)
         info['bdate'] = bdate
+        info['bdate_age'] = None
     return info
 
 
 def push_vk_info_to_at(airtable, info):
     return airtable.insert({
-        'Имя': info['name'],
-        'Ссылка на профиль': info['link'],
-        'Мобильный телефон': info['mobile_phone'],
-        'Домашний телефон': info['home_phone'],
+        'ФИО': info['name'],
+        'Источник': 'ВК Высота',
+        'Профиль в соцсети': info['link'],
+        'Телефон': info['phone'],
         'Дата рождения': info['bdate'],
         'Город': info['city'],
-        'Дата добавления': info['add_time']
-    })
+    }, typecast=True)['id']
 
 
 def main():
-    vk = vk_auth('*login*', '*password*')
-    airtable = at_auth('*base_id*', '*table_name*', '*api_key*')
-    friends = parse_table(airtable)
+    vk = vk_auth(
+        '', '')
+    airtable = at_auth('',
+                       '', '')
+    friends = parse_table(airtable, vk)
+    print('Table parsed')
     while True:
-        update_friends(vk, airtable, get_friend_ids(vk), friends)
+        update_friends(vk, airtable, get_all_friend_ids(vk), friends)
 
 
 if __name__ == '__main__':
